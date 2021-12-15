8eb0108ff639b78aafed87147cc7faf33cb1671d
pablo michelini <pab.michelini@gmail.com>
2021-12-15 18:55:55 +0100

    action_text added

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
   include Visible
   has_many :comments
+  has_rich_text :body
 
   validates :title, :image_url, presence: true
   validates :body, presence: true, length: { minimum: 10 }
